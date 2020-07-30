c8f91c0fb181341f85144b8c3b1e2e5569765724
fytang <1467827322@qq.com>
2020-07-30 21:20:35 +0800

    部分knn代码及数据

---
+++ KNN/kNN.py
@@ -0,0 +1,52 @@
+from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
+import operator
+
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ["A", "A", "B", "B"]
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDiatance = sqDiffMat.sum(axis=1)
+    distances = sqDiatance ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortClassCount[0][0]
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+filename = "./datingTestSet2.txt"
+a, b = file2matrix(filename)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(a[:, 1], a[:, 2])
+plt.show()
+
+
+
+
